4e8c996bbc7d869db68dbdcf983096e2f4183596
DevEmad007 <FarshidIbtihaj@gmail.com>
2023-10-31 16:18:41 +0600

    Remove unnecessery code

--- src/components/SmallBox.jsx
+++ src/components/SmallBox.jsx
@@ -8,73 +8,13 @@ const SmallBox = ({ bigBoxValue,id: boxID,resetCell }) => {
     const db = getFirestore();
     const [ smallBox,setSmallBox ] = useState(Array(9).fill(null));
     const {
-        setBigBox,
         XsTurn,
         setXsTurn,
-        smallBoxID,
-        cellid,
         CheckBox,
         isTwistModeOn,
         CheckBoxTwisted,
         checkWinner,
-        nxtPlayBox,
-        roomID,
-        playersIn,
-        isOnlinePlaying,
-        roomData } = useGameContext();
-
-    const updateRoom = async (cellID) => {
-        const roomRef = doc(db,"room",roomID.toString());
-        try {
-            if (cellID === 0) {
-                await updateDoc(roomRef,{
-                    'smallBox.id0': smallBox
-                });
-            }
-            else if (cellID == 1) {
-                await updateDoc(roomRef,{
-                    'smallBox.id1': smallBox
-                });
-            }
-            else if (cellID == 2) {
-                await updateDoc(roomRef,{
-                    'smallBox.id2': smallBox
-                });
-            }
-            else if (cellID == 3) {
-                await updateDoc(roomRef,{
-                    'smallBox.id3': smallBox
-                });
-            }
-            else if (cellID == 4) {
-                await updateDoc(roomRef,{
-                    'smallBox.id4': smallBox
-                });
-            }
-            else if (cellID == 5) {
-                await updateDoc(roomRef,{
-                    'smallBox.id5': smallBox
-                });
-            }
-            else if (cellID == 6) {
-                await updateDoc(roomRef,{
-                    'smallBox.id6': smallBox
-                });
-            }
-            else if (cellID == 7) {
-                await updateDoc(roomRef,{
-                    'smallBox.id7': smallBox
-                });
-            }
-            else if (cellID == 8) {
-                await updateDoc(roomRef,{
-                    'smallBox.id8': smallBox
-                });
-            }
-        } catch (error) {
-            console.log(error);
-        }
-    };
+        nxtPlayBox,} = useGameContext();
 
     useSkipRender(() => {
         setSmallBox(Array(9).fill(null));
@@ -114,24 +54,7 @@ const SmallBox = ({ bigBoxValue,id: boxID,resetCell }) => {
 
     useEffect(() => {
         checkWinner(smallBox);
-        //check if the gameBox git winner
-        if (isOnlinePlaying) {
-            updateRoom(smallBoxID);
-        }
     },[ smallBox ]);
-
-    useEffect(() => {
-        if (isOnlinePlaying && roomData !== undefined || null) {
-            // setBigBox(roomData?.bigBox);
-            // if (smallBoxID === 0) {
-            //     setSmallBox(roomData?.smallBox?.id0);
-            // }
-            // else if (smallBoxID === 1) {
-            //     setSmallBox(roomData?.smallBox?.id1);
-            // }
-        }
-        console.log('first');
-    },[ XsTurn ]);
 
     return (
         <div className='gameBox' >
